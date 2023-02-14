82d65fe442a80bed298a7c05bdbfae8769d8b7c7
mule <belmeetmule@gmail.com>
2023-02-14 18:09:39 +0300

    fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
-  validates :preparation_time, presence: true, numericality: {greater_than_or_equal_to: 0}
-  validates :cooking_time, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
 
   belongs_to :user
