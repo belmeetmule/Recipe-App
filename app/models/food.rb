1d30a2eb40685582dc51f85e88dd22a0b65987a6
mule <belmeetmule@gmail.com>
2023-02-14 15:37:58 +0300

    add validation for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
+  validates :name, precense: true
+  validates :measurement_unit, precense: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer, greater_than_or_equal_to: 0 }
+
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 end
