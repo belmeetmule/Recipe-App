e9bce42717e58606fc44458bc3ae71c83d66144b
mule <belmeetmule@gmail.com>
2023-02-14 16:02:38 +0300

    add validation for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
+  validates :name, precense: true
+  validates :preparation_time, precense: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :cooking_time, precense: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :description, presence: true
+  
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 end
