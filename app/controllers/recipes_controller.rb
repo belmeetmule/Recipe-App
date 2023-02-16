f69b6a917813649000d83e3acc302a417b405ed9
Elijah Odjegba <elijahodjegba@gmail.com>
2023-02-16 22:48:52 +0100

    Fix rubocop errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,42 +1,40 @@
 class RecipesController < ApplicationController
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
 
-    # GET /recipes/new
-    def new
-        @recipe = Recipe.new
-      end
-
-     # POST /recipes or /recipes.json
-     def create
-        @user = current_user
-        @recipe = @user.recipes.new(recipes_params)
-        if @recipe.save
-            redirect_to recipes_path, notice: 'recipe was successfully created'
-          else
-            render :new, alert: "Couldn't create recipe for user"
-          end
-      end
-  
-    # GET /recipes or /recipes.json
-    def index
-      @recipes = Recipe.all
-    end
-  
-    # GET /recipes/1 or /recipes/1.json
-    def show;
-        @recipe = Recipe.find(params[:id])    
-    end
-  
-    # DELETE /recipes/1 or /recipes/1.json
-    def destroy
-      @recipe = Recipe.find(params[:id])
-      @recipe.destroy
-    end
-  
-    private
-  
-    # Only allow a list of trusted parameters through.
-    def recipe_params
-      params.permit(:name, :description, current_user.id)
+  # POST /recipes or /recipes.json
+  def create
+    @user = current_user
+    @recipe = @user.recipes.new(recipes_params)
+    if @recipe.save
+      redirect_to recipes_path, notice: 'recipe was successfully created'
+    else
+      render :new, alert: "Couldn't create recipe for user"
     end
   end
-  
+
+  # GET /recipes or /recipes.json
+  def index
+    @recipes = Recipe.all
+  end
+
+  # GET /recipes/1 or /recipes/1.json
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  # DELETE /recipes/1 or /recipes/1.json
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def recipe_params
+    params.permit(:name, :description, current_user.id)
+  end
+end
