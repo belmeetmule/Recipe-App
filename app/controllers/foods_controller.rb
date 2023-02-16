c2c4fe336c9a880009b7f637c6c727d7e2d586f4
mule <belmeetmule@gmail.com>
2023-02-17 00:11:01 +0300

    fix rubocop errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,46 +1,45 @@
 class FoodsController < ApplicationController
-    def index
-       # @user = User.find(params[:user_id])
-        @foods = Food.all
-        render :index
+  def index
+    # @user = User.find(params[:user_id])
+    @foods = Food.all
+    render :index
+  end
+
+  def new
+    @food = Food.new
+    render :new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @food = @user.foods.new(**food_params)
+    if @food.save
+      flash[:success] = 'Food saved successfully'
+      redirect_to user_foods_url
+    else
+      flash.now[:error] = 'Error: Food could not be saved'
+      render :new
     end
+  end
 
-    def new
-        @food = Food.new
-        render :new
-    end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
 
-    def create
-        @user = User.find(params[:user_id])
-        @food = @user.foods.new(**food_params)
-        if @food.save
-            flash[:success] = 'Food saved successfully'
-            redirect_to user_foods_url
-        else
-            flash.now[:error] = 'Error: Food could not be saved'
-            render :new
-        end
-    end
+  def show
+    @food = Food.find(params[:id])
+  end
 
-    def destroy
-        @food = Food.find(params[:id])
-        @food.destroy
-        redirect_to foods_path
-    end
+  def edit
+    @food = Food.find(params[:id])
+    render :edit
+  end
 
-    def show
-        @food = Food.find(params[:id])
-    end
+  private
 
-    def edit
-        @food = Food.find(params[:id])
-        render :edit
-    end
-
-    private
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
-    end
-
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
+  end
 end
