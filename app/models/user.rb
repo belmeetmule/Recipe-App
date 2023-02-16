cb22ba26985715c0d93c6cd5efc54fdd6ee626c1
mule <belmeetmule@gmail.com>
2023-02-16 11:43:23 +0300

    Add device model to usr class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   validates :name, presence: true
 
   has_many :foods, dependent: :destroy
